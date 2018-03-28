86a8576829b467acdb6ca5be43a0ea1538fded5f
sboyd <sboyd@csumb.edu>
2018-03-28 05:20:06 +0000

    test

--- index.php
+++ index.php
@@ -194,7 +194,7 @@
                     {
                         if(isset($_GET[$result_movie[$i][0] . '_x']) && isset($_GET[$result_movie[$i][0] . '_y']))
                         {
-                        
+                            var_dump($result_movie[$i][0]);
                             $_SESSION['movie'] = $result_movie[$i];
                             $host  = $_SERVER['HTTP_HOST'];
                             $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
